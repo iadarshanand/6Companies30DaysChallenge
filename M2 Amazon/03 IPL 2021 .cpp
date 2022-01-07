249d9cdba34d0229306bedd0a6483e9d25ee9bd4
iadarshanand <anandadarsh2511@gmail.com>
2022-01-08 01:35:50 +0530

    Create 03 IPL 2021 .cpp

---
+++ M2 Amazon/03 IPL 2021 .cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> a, int n, int k) 
+    {
+        // your code here
+        multiset<int>ms;
+        for(int i=0;i<k;i++)
+        {
+            ms.insert(a[i]);
+        }
+        vector<int>ans;
+        ans.push_back(*ms.rbegin());
+        
+        for(int i=k;i<n;i++)
+        {
+            auto it=ms.find(a[i-k]);
+            ms.erase(it);
+            ms.insert(a[i]);
+            ans.push_back(*ms.rbegin());
+        }
+        return ans;
+    }
+};
